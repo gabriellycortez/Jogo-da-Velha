522d41091b7389e703d47cd518d4196c58f76b26
Gabrielly Cortez <cortezgabrielly@gmail.com>
2023-10-25 20:49:46 -0300

    adicionando JS

---
+++ scripts.js
@@ -0,0 +1,84 @@
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const winningMessage = document.querySelector('[data-winning-message]');
+
+let isCircleTurn; 
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+];
+
+const startGame = () => {
+    isCircleTurn = false;
+
+    for (const cell of cellElements) {
+        cell.addEventListener("click", handleClick, {once:true});
+    }
+ 
+   board.classList.add("x");
+
+}; 
+
+const endGame = (isDraw) => {
+    if (isDraw) {
+        winningMessageTextElement.innerText = 'Empate!';
+    } else {
+        winningMessageTextElement.innerText = isCircleTurn ? 'Cículo Venceu!' : 'X Venceu';
+    }
+
+    winningMessage.classList.add("show-winning-menssage"); 
+
+};
+
+const checkForWin = (currentPlayer) => {
+    return winningCombinations.some(combination => {
+        return combination.every(index =>{
+            return cellElements[index].classList.contains(currentPlayer);
+        });
+    });
+};
+
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+};
+
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn;
+
+    board.classList.remove('circle');
+    board.classList.remove('x');
+
+    if (isCircleTurn) {
+        board.classList.add('circle'); 
+    } else {
+        board.classList.add('x');
+    }
+};
+
+const handleClick = (e) => {
+    //Colocar a marca (X ou O)
+    const cell = e.target;
+    const classToAdd = isCircleTurn ? "circle" :  "x";
+    cell.classList.add(classToAdd);
+
+    //Verificar por vitória 
+    const isWin = checkForWin(currentPlayer);
+    if (isWin) {
+        endGame(false)
+    }
+    //Verificar por empate
+    //Mudar símbolo
+
+    swapTurns();
+};
+
+startGame();
